3c96e10b5986e25b140d80fcc64f6daa925edb8f
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2021-07-12 15:51:47 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from pprint import pprint
 import requests
 
-city_name = input('Name of city: ')
+import json
 
-state_code = input('State code: ')
+city_name = input("Name of city: ")
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q={},{}&appid=233fb10f061739fe6ced65c5f60ec5da&units=imperial'.format(city_name,state_code)
-
-print(url)
+url = "http://api.openweathermap.org/data/2.5/weather?q={}&appid=233fb10f061739fe6ced65c5f60ec5da".format(city_name)
 
 weather_data = requests.get(url).json()
+country_code = weather_data["sys"]["country"]
+
+if country_code == "US" or country_code == "LR" or country_code == "MM":
+    url+="&units=imperial"
+else:
+    url+="&units=metric"
+
+print(url)
+weather_data = requests.get(url).json()
+
+_json = open("countrycodes.json","r") #countrycodes is a json I created utilizing the data from https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json
+countrycodes = json.loads(_json.read())
+
+country_name = countrycodes[country_code]
+print(weather_data['name']+"\n"+country_name)
 
 pprint(weather_data)
-
-print("Hello World!")
