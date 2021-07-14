fda0a2d35cb54207a4647c6b4a01f65861563e0b
Paul Kuruvila <paul.paulkuruvila@gmail.com>
2021-07-14 02:29:19 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,25 +13,30 @@ root.maxsize(height=1150,width=1050)
 root.minsize(height=550,width=968)
 
 button_location = 1010
-searchButtonx = 847
+searchButtonx = 686
 def resizeWindow(e):
     dif = e.width-button_location
     if dif<40:
-        change_in_x = 847-((40-dif)/2)
+        change_in_x = 686-((40-dif)/2)
         if not dif<-43:
-            searchButton.place(x=change_in_x, y=100)
+            searchButton.place(x=change_in_x, y=102)
     elif dif>40:
         change_in_x = (dif-40)/2
-        searchButton.place(x=searchButtonx+change_in_x,y=100)
+        searchButton.place(x=searchButtonx+change_in_x,y=102)
     else:
-        searchButton.place(x=847, y=100)
+        searchButton.place(x=686, y=102)
 
 root.bind('<Configure>', resizeWindow)
+
+# function will take in current entered value from search box and return a list of suggested values
+def searchSuggestions(e):
+    suggestions = []
+    return suggestions
 
 #while button pressed and text != "", display search suggestions
 justSelected = False
 justEmpty = True
-def searchSuggestions(e):
+def readBar(e):
     global justSelected
     global justEmpty
     # read what is typed
@@ -52,14 +57,11 @@ def searchSuggestions(e):
         #if :
         # don't suggest anything if a suggestion was just chosen
         if not justSelected:
-            suggestions_box.place(x=204,y=143)
+            suggestions_box.place(x=204,y=144)
             # Display what is being typed
             suggestions_box.insert(0, searchBar.get())
             if justEmpty:
                 print("trying to fix buggy error")
-                e.xview_scroll(1, tk.UNITS)
-        else:
-            suggestions_box.place_forget()
         justEmpty = False
 
 def searchCity(e):
@@ -80,28 +82,29 @@ def listSelect(e):
     suggestions_box.place_forget()
 
 # Title canvas and frame around search bar
-searchBorder = tk.Frame(root, bg="#7bdaeb",bd=-2)
+searchBorder = tk.Frame(root, bg="#F6f7f6",bd=-2)
 searchBorder.place(relwidth = 1,relheight=0.5)
-topcanvas = tk.Canvas(root, height = 85, width = 2555, bg = "#72d23e",bd=-2)
+##72d23e
+topcanvas = tk.Canvas(root, height = 85, width = 2555, bg = "#7bdaeb",bd=-2)
 AppTitle = topcanvas.create_text(525,45,text="UbiWeather", font=("Calibri", 36))
 topcanvas.pack()
 # Search bar and small margin above it
-searchBarTopMargin = tk.Canvas(root, height = 15, width = 2555, bg = "#7bdaeb",bd=-2)
+searchBarTopMargin = tk.Canvas(root, height = 15, width = 2555, bg = "#F6f7f6",bd=-2)
 searchBarTopMargin.pack()
-searchBar = Entry(root, width=40, justify=LEFT, relief="groove",font= ('Calibri',24),bg="#E9ede9")
-searchBar.pack()
-searchBar.bind("<KeyRelease>", searchSuggestions)
+searchBar = Entry(root, width=40, justify=LEFT, relief="ridge",font= ('Calibri',24),bg="#E9ede9")
+searchBar.pack(pady=0.5)
+searchBar.bind("<KeyRelease>", readBar)
 # Search Button
-searchButton = tk.Button(root, height=1,width=15,font=('Calibri',15),text="Search", cursor="hand2", bg="#Bec0be")
-searchButton.place(x=847,y=100)
+searchButton = tk.Button(root, height=1,width=15,relief="flat",font=('Calibri',15),text="Search", cursor="hand2", bg="#Bec0be")
+searchButton.place(x=686,y=102)
 #searchButton.bind("<KeyRelease>", searchCity)
 # Main canvas
-canvas = tk.Canvas(root, height = 1050, width = 2555, bg = "#7bdaeb",bd=-2)
+canvas = tk.Canvas(root, height = 1050, width = 2555, bg = "#F6f7f6",bd=-2)
 canvas.pack()
 # Suggestions box
-suggestions_box = Listbox(root, width=64,cursor="hand2", font=('Calibri',15), relief="raised", bg="#E9ede9")
+suggestions_box = Listbox(root, width=48,height=1,cursor="hand2", font=('Calibri',15), relief="raised", bg="#E9ede9",bd=0)
 suggestions_box.pack(expand="YES")
-suggestions_box.place(x=204,y=143)
+suggestions_box.place(x=204,y=144)
 suggestions_box.place_forget()
 suggestions_box.bind("<<ListboxSelect>>",listSelect)
 
